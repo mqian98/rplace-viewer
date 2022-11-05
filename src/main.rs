db78c5a6e5dabb0f968fc9c3bf161de4135122a3
Michael Qian <weikai.qian@gmail.com>
2022-11-05 03:32:39 -0500

    calculating amount of edits for each datapoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
 mod rplace;
-use std::{thread, time::Duration};
+use std::{thread, time::{Duration, Instant}};
 
 use speedy2d::Window;
 use rplace::{window::RedditPlaceWindowHandler, data::RPlaceDataReader, search::RPlaceDataset};
+
+pub struct RPlaceDataCounter {
+    pub counter: Vec<Vec<u32>>,
+}
+
+impl RPlaceDataCounter {
+    pub fn empty(size: usize) -> RPlaceDataCounter {
+        let mut counter = Vec::new();
+        for y in 0..size {
+            let mut row = Vec::new();
+            for x in 0..size {
+                row.push(0);
+            }
+            counter.push(row);
+        }
+
+        RPlaceDataCounter { 
+            counter,
+        }
+    }
+
+    pub fn increment(&mut self, x: usize, y: usize) {
+        self.counter[y][x] += 1;
+    }
+}
 
 fn run_visualizer(file_path: &str) {
     let window = Window::new_fullscreen_borderless("R/Place Renderer").unwrap();
@@ -14,21 +39,33 @@ fn iterate_data(file_path: &str) {
 
     let size = 2000;
     let mut history = 0;
-    let mut dataset = RPlaceDataset::empty(size);
+    let limit = 170_000_000;
+    let mut start_time = Instant::now();
+    //let mut dataset = RPlaceDataset::empty(size);
+    let mut counter = RPlaceDataCounter::empty(size);
     if let Some(reader) = RPlaceDataReader::new(file_path){
         let iterator = reader.into_iter();
-        for datapoint in iterator {
+        for datapoint in iterator.take(limit) {
             if history % 1000000 == 0 {
-                println!("At {} lines", history);
+                let duration = start_time.elapsed();
+                println!("At {} lines | Duration - {:?}", history, duration);
+                start_time = Instant::now();
             }
 
-            dataset.add(datapoint, datapoint.coordinate.x as usize, datapoint.coordinate.y as usize);
+            //dataset.add(datapoint, datapoint.coordinate.x as usize, datapoint.coordinate.y as usize);
+            counter.increment(datapoint.coordinate.x as usize, datapoint.coordinate.y as usize);
             history += 1;
         }
     }
-    
-    println!("Sleeping - History size: {}", history);
-    thread::sleep(Duration::from_secs(30));
+
+    let mut all_counts: Vec<&u32> = counter.counter.iter().flatten().collect::<Vec<&u32>>();
+    all_counts.sort_by(|&&a, &&b| b.cmp(&a));
+
+    for (i, count) in all_counts.iter().enumerate().take(100) {
+        println!("Most counts: {} {}", i, count);
+    }
+    //println!("Sleeping - History size: {}", history);
+    //thread::sleep(Duration::from_secs(30));
 }
 
 fn main() {
