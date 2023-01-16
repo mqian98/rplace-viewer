ecf46e30ab9b9ee65c4209441f7bf570a0bceed4
Michael Qian <weikai.qian@gmail.com>
2023-01-16 16:43:25 -0600

    Added basic cors

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 use rplace::api::{index, game};
-use rocket::{launch, routes};
+use rocket::{launch, routes, http::Header, fairing::{Fairing, Info, Kind}, Request, Response};
 mod rplace;
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
         .mount("/game", routes![game])
+        .attach(Cors)
 }
 
 // mod rplace;
